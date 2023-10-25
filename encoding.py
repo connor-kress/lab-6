45a74818d36d0dec5c37c08e8520a5438638e6ca
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2023-10-25 17:35:28 -0400

    added decoding function

--- encoding.py
+++ encoding.py
@@ -17,8 +17,8 @@ def encode(string: str) -> str:
     return ''.join(str((int(c)+3) % 10) for c in string)
 
 
-# def decode(string: str) -> str:
-#     return ''.join(str((int(c)-3) % 10) for c in string)
+def decode(string: str) -> str:
+    return ''.join(str((int(c)-3) % 10) for c in string)
 
 
 def main() -> None:
